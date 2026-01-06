cb9ca2328ec7b7e8dbd2c55a4d61463c8d156705
subhinkrishna-s <subhinkrishna.suresh@gmail.com>
2026-01-06 16:12:37 +0530

    included gender fields for user

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -47,9 +47,9 @@ AuthRouter.post('/login', async(req, res)=>{
 
 AuthRouter.post('/register', async (req, res)=>{
     try{
-        const {fullname, email, contact, password, age} = req.body
+        const {fullname, email, contact, password, age, gender} = req.body
 
-        if(!fullname || !email || !contact || !password || !age){
+        if(!fullname || !email || !contact || !password ){
             return res.send({success: false, message: 'Please provide all details!'})
         }
         
@@ -66,15 +66,32 @@ AuthRouter.post('/register', async (req, res)=>{
         }else{ 
             userId = 1
         }
-
-        const newUser = new UserModel({
+        
+        let tempUser = {
             id: userId,
             fullname: fullname,
             email: email,
             contact: contact,
-            password: password,
-            age:age
-        })
+            password: password
+        }
+
+        if(age){
+            if (typeof age === 'number') {
+                tempUser.age = age
+            }
+            else if (age.match(/^\d+(\.\d+)?$/)) {
+                tempUser.age = parseFloat(age)
+            }
+            else {
+                return res.send({ success: false, message: 'Age must be a number.' })
+            }
+        }
+
+        if(gender && ['male', 'female', 'other'].includes(gender.toLowerCase().trim())){
+            tempUser.gender = gender.toLowerCase().trim()
+        }
+
+        const newUser = new UserModel(tempUser)
 
         const saveUser = await newUser.save()
 
